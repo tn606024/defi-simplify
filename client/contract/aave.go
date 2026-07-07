7ba5136335e2b4b8f05defe7a06692a1a875cfa7
tn606024 <tn606024@gmail.com>
2026-07-07 15:03:28 +0800

    refactor: move Aave read helpers

--- client/contract/aave.go
+++ client/contract/aave.go
@@ -3,7 +3,6 @@ package contract
 import (
 	"context"
 	_ "embed"
-	"fmt"
 	"math/big"
 	"strings"
 
@@ -51,72 +50,6 @@ type DataTypesUserReserveData struct {
 type TokenReserveData struct {
 	TokenAddress    common.Address
 	UserReserveData DataTypesUserReserveData
-}
-
-func getReserveData(conn EthereumClient, action *GetReserveDataAction) (*aave.DataTypesReserveData, error) {
-	pool, err := aave.NewPool(action.poolAddress, conn)
-	if err != nil {
-		return nil, err
-	}
-	reserveData, err := pool.GetReserveData(nil, action.asset)
-	if err != nil {
-		fmt.Println("Error getting reserve data:", err)
-		return nil, err
-	}
-	return &reserveData, nil
-}
-
-func getUserAccountData(conn EthereumClient, action *GetUserAccountDataAction) (*DataTypesUserAccountData, error) {
-	pool, err := aave.NewPool(action.poolAddress, conn)
-	if err != nil {
-		return nil, err
-	}
-	userAccountData, err := pool.GetUserAccountData(nil, action.user)
-	if err != nil {
-		return nil, err
-	}
-	return &DataTypesUserAccountData{
-		TotalCollateralBase:         userAccountData.TotalCollateralBase,
-		TotalDebtBase:               userAccountData.TotalDebtBase,
-		AvailableBorrowsBase:        userAccountData.AvailableBorrowsBase,
-		CurrentLiquidationThreshold: userAccountData.CurrentLiquidationThreshold,
-		Ltv:                         userAccountData.Ltv,
-		HealthFactor:                userAccountData.HealthFactor,
-	}, nil
-}
-
-func getAllReservesTokens(conn EthereumClient, action *GetAllReservesTokensAction) ([]aave.IPoolDataProviderTokenData, error) {
-	protocolDataProvider, err := aave.NewAaveProtocolDataProvider(action.protocolDataProviderAddress, conn)
-	if err != nil {
-		return nil, err
-	}
-	allReservesToken, err := protocolDataProvider.GetAllReservesTokens(nil)
-	if err != nil {
-		return nil, err
-	}
-	return allReservesToken, nil
-}
-
-func getUserReserveData(conn EthereumClient, action *GetUserReserveDataAction) (*DataTypesUserReserveData, error) {
-	protocolDataProvider, err := aave.NewAaveProtocolDataProvider(action.protocolDataProviderAddress, conn)
-	if err != nil {
-		return nil, err
-	}
-	userReserveData, err := protocolDataProvider.GetUserReserveData(nil, action.user, action.asset)
-	if err != nil {
-		return nil, err
-	}
-	return &DataTypesUserReserveData{
-		CurrentATokenBalance:     userReserveData.CurrentATokenBalance,
-		CurrentStableDebt:        userReserveData.CurrentStableDebt,
-		CurrentVariableDebt:      userReserveData.CurrentVariableDebt,
-		PrincipalStableDebt:      userReserveData.PrincipalStableDebt,
-		ScaledVariableDebt:       userReserveData.ScaledVariableDebt,
-		StableBorrowRate:         userReserveData.StableBorrowRate,
-		LiquidityRate:            userReserveData.LiquidityRate,
-		StableRateLastUpdated:    userReserveData.StableRateLastUpdated,
-		UsageAsCollateralEnabled: userReserveData.UsageAsCollateralEnabled,
-	}, nil
 }
 
 // 6. Transaction creation functions
